10f0dad562d0ad1ab1e542685b8016001bb125fd
OlaffoJaszczur <olafsenisson@gmail.com>
2025-01-16 12:17:55 +0100

    jest polaczenie, teraz cos mi powiedz

--- connecton_test.py
+++ connecton_test.py
@@ -1,15 +1,30 @@
 import serial
 import keyboard
+import time
 
 # Initialize the serial connection
-ser = serial.Serial('USART2', 38400)
+ser = serial.Serial('/dev/ttyUSB0', 38400)
+
+# try:
+#     print("Press any key to stop...")
+#     while not keyboard.is_pressed(hotkey='esc'):
+#         if ser.in_waiting > 0:
+#             data = ser.read(ser.in_waiting)
+#             print(data.decode('utf-8'), end='')
+# finally:
+#     ser.close()
+#     print("\nSerial connection closed.")
+
+start_time = time.time()
+elapsed_time = 0
 
 try:
-    print("Press any key to stop...")
-    while not keyboard.is_pressed():
+    print("Reading data for 5 seconds...")
+    while elapsed_time < 5:
         if ser.in_waiting > 0:
             data = ser.read(ser.in_waiting)
-            print(data.decode('utf-8'), end='')
+            print(data.decode('US-ASCII'), end='')
+        elapsed_time = time.time() - start_time
 finally:
     ser.close()
-    print("\nSerial connection closed.")
+    print(f"\nSerial connection closed. Elapsed time: {elapsed_time:.2f} seconds.")
